2b6a1105d7d33dde3664d77d83226e9c6b5fa6f1
Saif <saifsmailbox98@yahoo.in>
2019-03-29 19:18:53 +0530

    downlaoded all images to local

--- gallery.js
+++ gallery.js
@@ -1,20 +1,20 @@
 module.exports = {
     "pictures":[
-        "https://imgur.com/UlhGsRG.jpg",
-        "https://imgur.com/SmrwU6z.jpg",
-        "https://imgur.com/F2dQnMQ.jpg",
-        "https://imgur.com/9dhkI2E.jpg",
-        "https://imgur.com/G0j0bwf.jpg",
-        "https://imgur.com/ciUo43C.jpg",
-        "https://imgur.com/ZYFnTHz.jpg",
-        "https://imgur.com/WloyG2H.jpg",
-        "https://imgur.com/Ky62d2y.jpg",
-        "https://imgur.com/ddwzTLK.jpg",
-        "https://imgur.com/4inBul5.jpg",
-        "https://imgur.com/K01F0PN.jpg",
-        "https://imgur.com/2Ld2b3Y.jpg",
-        "https://imgur.com/4XghpP4.jpg",
-        "https://imgur.com/fReIS1k.jpg"
+        "images/UlhGsRG.jpg",
+        "images/SmrwU6z.jpg",
+        "images/F2dQnMQ.jpg",
+        "images/9dhkI2E.jpg",
+        "images/G0j0bwf.jpg",
+        "images/ciUo43C.jpg",
+        "images/ZYFnTHz.jpg",
+        "images/WloyG2H.jpg",
+        "images/Ky62d2y.jpg",
+        "images/ddwzTLK.jpg",
+        "images/4inBul5.jpg",
+        "images/K01F0PN.jpg",
+        "images/2Ld2b3Y.jpg",
+        "images/4XghpP4.jpg",
+        "images/fReIS1k.jpg"
 
     ]
 };
